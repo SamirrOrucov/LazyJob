c8ae0b80e0773ad323929215a53f26526a506425
Samir Orucov <samiroruclu10@gmail.com>
2024-11-09 16:39:05 +0400

    structure created

---
+++ apps/admin/src/index.jsx
@@ -0,0 +1,9 @@
+// apps/admin/src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Admin Panel Çalışıyor!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ apps/web/pages/index.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+    return <h1>Next.js Website Çalışıyor!</h1>;
+  }
